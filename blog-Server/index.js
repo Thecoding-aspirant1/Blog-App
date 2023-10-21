9e08f9f52ff16df0f5078052e28a2251e1242ad1
suhani <sahusuhani14@gmail.com>
2023-10-21 13:53:02 +0530

    changes

--- blog-Server/index.js
+++ blog-Server/index.js
@@ -3,7 +3,7 @@ const app=express()
 
 app.route("/").get((req,res)=>res.json("your first rest api 1"));
 
-const port = 5002; // Change to an available port
+const port = 5002;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
